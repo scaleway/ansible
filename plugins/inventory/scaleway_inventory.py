c7f648bc3149afe6906e7b9c5eca24fbd43016d0
Laure Masson <lmasson@scaleway.com>
2025-09-24 16:01:04 +0200

    add ElasticMetal popuplate_network

--- plugins/inventory/scaleway_inventory.py
+++ plugins/inventory/scaleway_inventory.py
@@ -1,7 +1,9 @@
 from dataclasses import field, dataclass
 from abc import ABC, abstractmethod
+from dataclasses import dataclass, field
+from typing import List
 
-from scaleway.ipam.v1 import IpamV1API, IP
+from scaleway.k8s.v1.tests.test_k8s_sk import private_network
 
 _ALLOWED_FILE_NAME_SUFFIXES = (
     "scaleway.yaml",
@@ -16,12 +18,14 @@ try:
     from scaleway.applesilicon.v1alpha1 import ApplesiliconV1Alpha1API, ApplesiliconV1Alpha1PrivateNetworkAPI, \
     ServerPrivateNetworkStatus
     from scaleway.applesilicon.v1alpha1 import Server as ApplesiliconServer
-    from scaleway.baremetal.v1 import BaremetalV1API, IPVersion as BaremetalIPVersion
+    from scaleway.baremetal.v1 import BaremetalV1API, IPVersion as BaremetalIPVersion, IPVersion, \
+    BaremetalV1PrivateNetworkAPI
     from scaleway.baremetal.v1 import Server as BaremetalServer
     from scaleway.instance.v1 import InstanceV1API, ServerState
     from scaleway.instance.v1 import Server as InstanceServer
     from scaleway.dedibox.v1 import DediboxV1API, IPVersion as DediboxIPVersion
     from scaleway.dedibox.v1 import ServerSummary as DediboxServer
+    from scaleway.ipam.v1 import IpamV1API, IP
 
     HAS_SCALEWAY_SDK = True
 except ImportError:
@@ -45,26 +49,6 @@ class _Host(ABC):
         ...
     """Extract IPs/DNS from the Scaleway SDK object."""
 
-@dataclass
-class _InstanceHost(_Host):
-    public_dns: str | None = None
-    private_dns: str | None = None
-
-    def populate_network(self, server: "InstanceServer", client: "Client"):
-        if server.public_ip:
-            self.public_ipv4.append(server.public_ip.address)
-        if server.private_ip:
-            self.private_ipv4.append(server.private_ip)
-        if server.ipv6:
-            self.public_ipv6.append(server.ipv6.address)
-
-        # DNS
-        self.public_dns = f"{server.id}.pub.instances.scw.cloud"
-        self.private_dns = f"{server.id}.priv.instances.scw.cloud"
-
-
-from dataclasses import dataclass, field
-from typing import List
 
 @dataclass
 class _ApplesiliconHost(_Host):
@@ -93,10 +77,52 @@ class _InstanceServerHost(_Host):
     public_dns: str | None = None
     private_dns: str | None = None
     def populate_network(self, server: "InstanceServer", client: "Client") -> None:
-        if server.public_ip:
-            self.public_ipv4.append(server.public_ip.address)
-        if server.private_ip:
-            if server.private_ip
+        self.public_dns = f"{server.id}.pub.instances.scw.cloud"
+        self.private_dns = f"{server.id}.priv.instances.scw.cloud"
+        if server.public_ips:
+            for ip in server.public_ips:
+                if ip.family == "ipv4":
+                    self.public_ipv4.append(ip.address)
+                else:
+                    self.public_ipv6.append(ip.address)
+
+        ipam_api = IpamV1API(client=client)
+
+        ips: List[IP] = [
+            ip
+            for pn in server.private_nics
+            for ip in ipam_api.list_i_ps_all(resource_id=pn.id, attached=True)
+        ]
+
+        self.private_ipv4.extend(ip.address.split("/")[0] for ip in ips if not ip.is_ipv6)
+        self.private_ipv6.extend(ip.address.split("/")[0] for ip in ips if ip.is_ipv6)
+
+
+class _ElasticMetalHost(_Host):
+    def populate_network(self, server: "BaremetalServer", client: "Client") -> None:
+        for ip in server.ips or []:
+            target_list = self.public_ipv4 if ip.version.lower() == "ipv4" else self.public_ipv6
+            target_list.append(ip.address)
+
+        baremetal_pn_api = BaremetalV1PrivateNetworkAPI(client=client)
+        private_networks = baremetal_pn_api.list_server_private_networks_all(server_id=server.id)
+        ipam_api = IpamV1API(client=client)
+        ips: List[IP] = [
+            ip
+            for pn in private_networks
+            for ip in ipam_api.list_i_ps_all(resource_id=pn.id, attached=True)
+        ]
+
+        self.private_ipv4.extend(ip.address.split("/")[0] for ip in ips if not ip.is_ipv6)
+        self.private_ipv6.extend(ip.address.split("/")[0] for ip in ips if ip.is_ipv6)
+
+
+
+
+
+
+
+
 
 
 
