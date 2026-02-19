21a7c954d7300eade8d8773ce3db2886a5b25dc7
Guillaume Noale <gnoale@scaleway.com>
2026-02-19 11:06:51 +0100

    fix: get instance ips (#97)

--- plugins/module_utils/instance.py
+++ plugins/module_utils/instance.py
@@ -5,39 +5,34 @@ try:
     from scaleway import Client
     from scaleway.instance.v1 import Server as InstanceServer
     from scaleway.ipam.v1 import IpamV1API
-    from scaleway.vpc.v2 import VpcV2API
 except ImportError:
     HAS_SCALEWAY_SDK = False
 
 
 @dataclass
-class VpcIps:
+class VpcIp:
     ipv4: Optional[str] = None
     ipv6: Optional[str] = None
 
 
 def get_instance_private_ips(
     client: "Client", servers: list["InstanceServer"]
-) -> Dict[str, VpcIps]:
+) -> Dict[str, VpcIp]:
     """
     return a mapping of first found vpc IPs with the instance
     """
-    vpc_api = VpcV2API(client)
     ipam_api = IpamV1API(client)
 
     vpc_ips = {}
 
     for server in servers:
+        vpc_ip = VpcIp()
+        vpc_ips[server.id] = vpc_ip
         for pnic in server.private_nics:
-            server_pn = vpc_api.get_private_network(
-                private_network_id=pnic.private_network_id
-            )
-            vpc_ips[server.id] = VpcIps
-
-            for ip in ipam_api.list_i_ps(vpc_id=server_pn.vpc_id).ips:
+            for ip in ipam_api.list_i_ps_all(resource_id=pnic.id):
                 if ip.is_ipv6:
-                    vpc_ips[server.id].ipv6 = ip.address.split("/")[0]
+                    vpc_ip.ipv6 = ip.address.split("/")[0]
                 else:
-                    vpc_ips[server.id].ipv4 = ip.address.split("/")[0]
+                    vpc_ip.ipv4 = ip.address.split("/")[0]
 
     return vpc_ips
