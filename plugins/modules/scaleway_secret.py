c619400789a7c2570e8db647618e6d985b5b8856
PendaGTP <38917281+PendaGTP@users.noreply.github.com>
2026-04-20 09:58:22 +0200

    fix(scw_secret): mark path as optional with default /

--- plugins/modules/scaleway_secret.py
+++ plugins/modules/scaleway_secret.py
@@ -34,7 +34,8 @@ options:
     path:
         description: path
         type: str
-        required: true
+        required: false
+        default: /
     project_id:
         description: project_id
         type: str
@@ -207,7 +208,7 @@ def main() -> None:
         dict(
             state=dict(type="str", default="present", choices=["absent", "present"]),
             name=dict(type="str", required=True),
-            path=dict(type="str", required=False),
+            path=dict(type="str", required=False, default="/"),
             project_id=dict(type="str", required=False),
             tags=dict(type="list", required=False, elements="str"),
             description=dict(type="str", required=False),
